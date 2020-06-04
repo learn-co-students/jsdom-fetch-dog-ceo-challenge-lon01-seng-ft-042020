a3da3a033bddb93bfbb1c0927385607771620422
jsymondst <jammiesymondstayler@gmail.com>
2020-06-04 16:04:11 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -15,8 +15,7 @@ document.addEventListener("DOMContentLoaded", function(event){
                const dogImagesDiv = document.getElementById("dog-image-container");
                dogImagesDiv.appendChild(imgElement);
             });
-        }
-        )
+        })
 
     fetch("https://dog.ceo/api/breeds/list/all")
         .then(parseJSON)
@@ -35,8 +34,6 @@ document.addEventListener("DOMContentLoaded", function(event){
             }
         })
 
-
-
         const selectALetterBox = document.getElementById("breed-dropdown");
         selectALetterBox.addEventListener("change", function(event){
             // Clear the list of all of the current breeds.
@@ -51,40 +48,17 @@ document.addEventListener("DOMContentLoaded", function(event){
                 .then(function(json){
                     for ( let [key, value] of Object.entries(json.message)){
                         if(key.startsWith(selectALetterBox.value)){
-
                             const breedLi = document.createElement("li");
                             breedLi.innerText = key;
                             breedLi.className = "breed-item"
                             breedLi.addEventListener("click", function(event){
                                 event.target.style.color = "MediumSeaGreen";
                                 
-                            })
-                            
+                            })                            
                             const breedListDiv = document.getElementById("dog-breeds");
                             breedListDiv.appendChild(breedLi);
                             }
                         }
                     })
         })
-
-
-function name(param){
-    body
-}
-
-const funcName(params) => {
-    body
-}
-
-
-
-
-        
-         
-        
-
-        // {"message":{"affenpinscher":[],"african":[],"airedale":[],"akita":[],"appenzeller":[],"australian":["shepherd"],"basenji":[],"beagle":[],"bluetick":[],"borzoi":[],"bouvier":[],"boxer":[],"brabancon":[],"briard":[],"buhund":["norwegian"],"bulldog":["boston","english","french"],"bullterrier":["staffordshire"],"cairn":[],"cattledog":["australian"],"chihuahua":[],"chow":[],"clumber":[],"cockapoo":[],"collie":["border"],"coonhound":[],"corgi":["cardigan"],"cotondetulear":[],"dachshund":[],"dalmatian":[],"dane":["great"],"deerhound":["scottish"],"dhole":[],"dingo":[],"doberman":[],"elkhound":["norwegian"],"entlebucher":[],"eskimo":[],"finnish":["lapphund"],"frise":["bichon"],"germanshepherd":[],"greyhound":["italian"],"groenendael":[],"havanese":[],"hound":["afghan","basset","blood","english","ibizan","plott","walker"],"husky":[],"keeshond":[],"kelpie":[],"komondor":[],"kuvasz":[],"labrador":[],"leonberg":[],"lhasa":[],"malamute":[],"malinois":[],"maltese":[],"mastiff":["bull","english","tibetan"],"mexicanhairless":[],"mix":[],"mountain":["bernese","swiss"],"newfoundland":[],"otterhound":[],"ovcharka":["caucasian"],"papillon":[],"pekinese":[],"pembroke":[],"pinscher":["miniature"],"pitbull":[],"pointer":["german","germanlonghair"],"pomeranian":[],"poodle":["miniature","standard","toy"],"pug":[],"puggle":[],"pyrenees":[],"redbone":[],"retriever":["chesapeake","curly","flatcoated","golden"],"ridgeback":["rhodesian"],"rottweiler":[],"saluki":[],"samoyed":[],"schipperke":[],"schnauzer":["giant","miniature"],"setter":["english","gordon","irish"],"sheepdog":["english","shetland"],"shiba":[],"shihtzu":[],"spaniel":["blenheim","brittany","cocker","irish","japanese","sussex","welsh"],"springer":["english"],"stbernard":[],"terrier":["american","australian","bedlington","border","dandie","fox","irish","kerryblue","lakeland","norfolk","norwich","patterdale","russell","scottish","sealyham","silky","tibetan","toy","westhighland","wheaten","yorkshire"],"vizsla":[],"waterdog":["spanish"],"weimaraner":[],"whippet":[],"wolfhound":["irish"]},"status":"success"}
-
-
-
 })
